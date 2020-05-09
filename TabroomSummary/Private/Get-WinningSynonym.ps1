cd80d2b9dd064bc43e6add61cf53dad8b27c46f6
benjmor <44407400+benjmor@users.noreply.github.com>
2020-05-09 10:50:01 -0500

    Create Get-WinningSynonym.ps1

---
+++ TabroomSummary/Private/Get-WinningSynonym.ps1
@@ -0,0 +1,25 @@
+<#
+    .Synopsis
+    Returns a random phrase from a list of phrases synonymous with winning. First letter is capitalized unless specified otherwise.
+#>
+function Get-WinningSynonym{
+  param(
+    [switch]$lowercase
+  )
+    $winningSynonymArray = @(
+        "Championing",
+        "Trimphant in",
+        "In first place in",
+        "Taking home top honors in",
+        "Placing first in",
+        "Victorious in",
+        "Besting all competition in",
+        "Crowned champion in",
+        "The tournament's winner in",
+        "The tournament's champion in"
+    )
+    if (!($lowercase)){
+      return $winningSynonymArray[(Get-Random -Minimum 0 -Maximum ($winningSynonymArray.Length))]
+    }
+    return ($winningSynonymArray[(Get-Random -Minimum 0 -Maximum ($winningSynonymArray.Length))]).ToLower()
+}
