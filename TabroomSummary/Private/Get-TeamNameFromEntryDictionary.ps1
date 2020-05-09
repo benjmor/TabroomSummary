546fdbabe893c146cb7bdf8f1bde9591343d490c
benjmor <44407400+benjmor@users.noreply.github.com>
2020-05-09 10:24:24 -0500

    Update Get-TeamNameFromEntryDictionary.ps1

--- TabroomSummary/Private/Get-TeamNameFromEntryDictionary.ps1
+++ TabroomSummary/Private/Get-TeamNameFromEntryDictionary.ps1
@@ -1,6 +1,11 @@
 <#
-    .Synopsis
-    Wrapper function to get Team Name
+  .Synopsis
+  Returns the team name from the given team object or team name.
+  .Notes
+  If $team is an object with an entry property on it, use that entry value to get the team name.
+  Otherwise, assume that the value of $team is an entry ID and use that to get the team name.
+  .Example
+  Get-TeamNameFromEntryDictionary -entryDictionary $tournamentEntries -team $myTeam
 #>
 function Get-TeamNameFromEntryDictionary{
     param(
