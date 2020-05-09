ff747e7711421e7f8d92cd94c104a45528bbb953
benjmor <44407400+benjmor@users.noreply.github.com>
2020-05-09 12:52:43 -0500

    Create Get-TOCBidListBySchool.ps1

---
+++ TabroomSummary/Private/Get-TOCBidListBySchool.ps1
@@ -0,0 +1,29 @@
+<#
+    .Synopsis
+    Given a school name, returns an array of team names/codes from that school that received a TOC bid.
+#>
+function Get-TOCBidListBySchool{
+    param(
+        $specificSchoolName,
+        $entryDictionary,
+        $jsonObject
+    )
+    $schoolSpecificEntries = Get-DebateEntriesBySchoolName -schoolName $specificSchoolName -entryDictionary $globalEntryDictionary
+    $bidObjectArray = @()
+    foreach ($division in $jsonObject.categories.events){
+        $TOCBids = $division.result_sets | where {$_.label -like "TOC Qualifying Bids"}
+        foreach ($bidResult in $TOCBids.results){
+            if ($schoolSpecificEntries.entry_id -contains $bidResult.entry){
+                $biddingTeamCode = Get-TeamCodeFromEntryDictionary -entryDictionary $globalEntryDictionary -team $bidResult
+                $biddingTeamName = Get-TeamNameFromEntryDictionary -entryDictionary $globalEntryDictionary -team $bidResult
+                $bidObject = [PsCustomObject]@{
+                    TeamCode = $biddingTeamCode;
+                    TeamName = $biddingTeamName;
+                    Division = $division.name     
+                }
+                $bidObjectArray += $bidObject
+            }
+        }
+    }
+    return $bidObjectArray
+}
