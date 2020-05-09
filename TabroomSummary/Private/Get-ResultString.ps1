94550e05e28485afd14ded9d428c818aa16665df
benjmor <44407400+benjmor@users.noreply.github.com>
2020-05-09 10:47:10 -0500

    Create Get-ResultString.ps1

---
+++ TabroomSummary/Private/Get-ResultString.ps1
@@ -0,0 +1,31 @@
+<#
+    .Synopsis
+    Function to generate an English-language sentence that describe a particular tournament result.
+    .Parameter resultType
+    Final Places, Prelim Seeds, Bracket, etc
+    .Parameter divName
+    name of the division -- Policy Debate, Congress, Dramatic Interpretation, etc.
+    .Parameter resultReportTeamName
+    Individual or team name -- "Andy ZZTestname", "Worlds School USA Blue", "Jones and Smith", etc.
+    .Parameter resultReportTeamCode
+    Team code -- "Test Academy ZZ", "DU Duolastname & Partner"
+#>
+function Get-ResultString{
+    param(
+        [string]$resultType,
+        [string]$divName,
+        [string]$resultReportTeamName,
+        [string]$resultReportTeamCode
+    )
+    if (($resultReportTeamName -like '* and *') -or ($resultReportTeamName -like '*&*')){
+        $isTeam = 1
+    }
+    $honorType = if ($resultType -eq "*Prelim*") {"top seed"} elseif ($resultType -eq "*Final Place*") {"champion"}
+
+    #Champions only
+    if ($honorType -eq "champion"){
+        return "$(Get-WinningSynonym) the $divName division was $(if ($isTeam){"the team of "})$resultReportTeamName ($resultReportTeamCode)."
+    } else {
+        return Get-NonChampionResultsString -honorType $honorType -divName $divName -isTeam $([bool]$isTeam) -TeamName $resultReportTeamName -TeamCode $resultReportTeamCode
+    }
+}
