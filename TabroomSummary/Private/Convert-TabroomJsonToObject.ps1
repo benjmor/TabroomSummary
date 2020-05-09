02e090debfc338887d7d80ca94d8a5b900c7a37d
benjmor <44407400+benjmor@users.noreply.github.com>
2020-05-09 10:14:00 -0500

    Create Convert-TabroomJsonToObject.ps1

---
+++ TabroomSummary/Private/Convert-TabroomJsonToObject.ps1
@@ -0,0 +1,29 @@
+<#
+    .Synopsis
+        Takes a tabroom-formatted Json and returns a PowerShell object representing the tournament data.
+    .Example
+        Convert-TabroomJsonToObject -tabroomJson "c:\temp\myJson.json"
+#>
+function Convert-TabroomJsonToObject{
+    param(
+        [Parameter(Mandatory=$true,
+        ParameterSetName="ImportByFilePath")]
+        [string]$tabroomJsonPath,
+        [Parameter(Mandatory=$true,
+        ParameterSetName="ImportText")]
+        [string]$jsonText
+    )
+    if ($tabroomJsonPath){
+        $jsonText = Get-Content $tabroomJsonPath
+    }
+    #region Renames some fields that have duplicates in different case (eg. EHm and Ehm). This is a requirement so that PowerShell's case-insensitive ConvertFrom-Json works.
+    $jsonTextReplaced = $jsonText.replace('"EHm"','"EHm_"')
+    $jsonTextReplaced = $jsonTextReplaced.replace('"Ehms"','"Ehms_"')
+    $jsonTextReplaced = $jsonTextReplaced.replace('"hmv"','"hmv_"')
+    $jsonTextReplaced = $jsonTextReplaced.replace('"hmsv"','"hmsv_"')
+    $jsonTextReplaced = $jsonTextReplaced.replace('"hms"','"hms_"')
+    $jsonTextReplaced = $jsonTextReplaced.replace('"hm"','"hm_"')
+    $jsonTextReplaced = $jsonTextReplaced.replace('"H"','"H_"')
+    #endregion
+    return ($jsonTextReplaced | ConvertFrom-Json)
+}
